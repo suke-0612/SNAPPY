d6ab84321ec3ff41ce6d2738829a4af9e6bf01ae
Fuki0612 <fukimax0612@gmail.com>
2025-08-09 13:37:44 +0900

    add: pagination_without_dots

---
+++ lib/components/pagination.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Pagination extends StatelessWidget {
+  final int currentPage;
+  final int totalPages;
+  final Function(int) onPageChanged;
+
+  const Pagination({
+    Key? key,
+    required this.currentPage,
+    required this.totalPages,
+    required this.onPageChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // 先頭のページに戻るボタン
+          if (currentPage > 1) ...[
+            GestureDetector(
+              onTap: () => onPageChanged(1),
+              child: const Icon(
+                Icons.keyboard_double_arrow_left,
+                color: Colors.black,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => onPageChanged(currentPage - 1),
+              child: const Icon(
+                Icons.keyboard_arrow_left,
+                color: Colors.black,
+              ),
+            ),
+          ] else
+            const SizedBox(width: 48), // スペースを確保
+
+          // 数字のリスト
+          ...List.generate(totalPages, (index) {
+            final pageIndex = index + 1;
+            return GestureDetector(
+              onTap: () => onPageChanged(pageIndex),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                width: 35,
+                height: 35,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color:
+                      currentPage == pageIndex ? Colors.blue : Colors.grey[300],
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                child: Text(
+                  '$pageIndex',
+                  style: TextStyle(
+                    color:
+                        currentPage == pageIndex ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          }),
+
+          // 次のページに進むボタン
+          if (currentPage < totalPages) ...[
+            GestureDetector(
+              onTap: () => onPageChanged(currentPage + 1),
+              child: const Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.black,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => onPageChanged(totalPages),
+              child: const Icon(
+                Icons.keyboard_double_arrow_right,
+                color: Colors.black,
+              ),
+            ),
+          ] else
+            const SizedBox(width: 48), // スペースを確保
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:snappy/importer.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentPage = 1;
+  int totalPages = 5;
+
+  void _onPageChanged(int page) {
+    setState(() {
+      currentPage = page;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const BaseScreen(
+    return BaseScreen(
       child: Column(
         children: [
           Expanded(
-            child: Center(
-              child: Text(
-                'ここにホームの内容',
-                style: TextStyle(color: Colors.white),
-              ),
+            child: Column(
+              children: [
+                Pagination(
+                  currentPage: currentPage,
+                  totalPages: totalPages,
+                  onPageChanged: _onPageChanged,
+                ),
+                Text(
+                  'This is page $currentPage',
+                ),
+              ],
             ),
           ),
         ],
